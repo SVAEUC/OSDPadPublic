04099bfe9a4fc6cf32d3ed64273f295dce6815aa
SVA EUC <116421578+SVAEUC@users.noreply.github.com>
2024-07-11 16:41:06 +0200

    Update Windows11_23H2_EN.ps1

--- Windows11_23H2_EN.ps1
+++ Windows11_23H2_EN.ps1
@@ -1,1 +1,15 @@
-xx
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Green "Setting Display Resolution to 1600x"
+    Set-DisRes 1600
+}
+
+$Params = @{
+    OSVersion = "Windows 11"
+    OSBuild = "23H2"
+    OSEdition = "Pro"
+    OSLanguage = "en-US"
+    OSLicense = "Retail"
+    ZTI = $true
+    Firmware = $true
+}
+Start-OSDCloud @Params -Restart
